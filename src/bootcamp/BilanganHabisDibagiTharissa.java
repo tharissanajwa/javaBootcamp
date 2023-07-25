39b34557e4a359289373aabcc16894a65e30e86f
Tharissa Najwa <tharissaazz@gmail.com>
2023-07-25 10:25:56 +0700

    memodifikasi file tahunKabisat karena seharusnya tidak memakai operator logika

---
+++ src/bootcamp/BilanganHabisDibagiTharissa.java
@@ -0,0 +1,14 @@
+package bootcamp;
+
+import java.util.Scanner;
+
+public class BilanganHabisDibagiTharissa {
+    public static void main(String[] args) {
+
+        // membuat scanner
+        Scanner scannerTharissa = new Scanner(System.in);
+
+        // input bilangan dari pengguna
+
+    }
+}
--- src/bootcamp/TahunKabisatTharissa.java
+++ src/bootcamp/TahunKabisatTharissa.java
@@ -6,25 +6,35 @@ public class TahunKabisatTharissa {
     public static void main(String[] args) {
 
         // membuat scanner
-        Scanner scanner = new Scanner(System.in);
+        Scanner scannerTharissa = new Scanner(System.in);
 
         // input tahun dari pengguna
         System.out.println("Masukkan tahun : ");
-        int tahunTharissa = scanner.nextInt();
+        int tahunTharissa = scannerTharissa.nextInt();
 
         // Panggil metode untuk melakukan perhitungan tahun kabisat
         hitungKabisatTharissa(tahunTharissa);
 
         // scanner ditutup
-        scanner.close();
+        scannerTharissa.close();
     }
 
     // membuat metode hitung kabisat dengan operasi relasional dan aritmatika
     private static void hitungKabisatTharissa(int tahunTharissa) {
-        if ((tahunTharissa % 4 == 0) && (tahunTharissa % 400 == 0) || (tahunTharissa % 100 != 0) ) {
-             System.out.println("Tahun " + tahunTharissa + " merupakan tahun kabisat");
+        if (tahunTharissa % 4 == 0) {
+            if (tahunTharissa % 100 == 0) {
+                System.out.println("Tahun " + tahunTharissa + " bukan tahun kabisat.");
+            } else {
+                System.out.println("Tahun " + tahunTharissa + " adalah tahun kabisat.");
+            }
+        } else if (tahunTharissa % 400 == 0) {
+            if (tahunTharissa % 100 == 0) {
+                System.out.println("Tahun " + tahunTharissa + " bukan tahun kabisat.");
+            } else {
+                System.out.println("Tahun " + tahunTharissa + " adalah tahun kabisat.");
+            }
         } else {
-            System.out.println("Tahun " + tahunTharissa + " bukan tahun kabisat");
+            System.out.println("Tahun " + tahunTharissa + " bukan tahun kabisat.");
         }
     }
 }
