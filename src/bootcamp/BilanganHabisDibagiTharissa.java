ff9dd68f8bbfa3798b2a396012b331d46d2c56b3
Tharissa Najwa <tharissaazz@gmail.com>
2023-07-25 10:33:05 +0700

    mengumpulkan tugas bilangan habis dibagi 3 dan 5

--- src/bootcamp/BilanganHabisDibagiTharissa.java
+++ src/bootcamp/BilanganHabisDibagiTharissa.java
@@ -9,6 +9,22 @@ public class BilanganHabisDibagiTharissa {
         Scanner scannerTharissa = new Scanner(System.in);
 
         // input bilangan dari pengguna
+        System.out.println("Masukkan bilangan : ");
+        int bilanganTharissa = scannerTharissa.nextInt();
 
+        // memanggil metode untuk melakukan perhitungan
+        hitungBilanganTharissa(bilanganTharissa);
+
+        // menutup scanner
+        scannerTharissa.close();
+
+    }
+
+    private static void hitungBilanganTharissa(int bilanganTharissa) {
+        if ((bilanganTharissa % 3 == 0) && (bilanganTharissa % 5 == 0)) {
+            System.out.println("Bilangan " + bilanganTharissa + " memenuhi kondisi.");
+        } else {
+            System.out.println("Bilangan " + bilanganTharissa + " tidak memenuhi kondisi.");
+        }
     }
 }
